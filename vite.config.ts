08c63a648ff4e8d1cb7d693bc5f3b53bfeabf3cc
nhachocbai1443-oss <nhachocbai1443@gmail.com>
2026-01-17 19:39:23 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/gmail/',
       server: {
         port: 3000,
         host: '0.0.0.0',
